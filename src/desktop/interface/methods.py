57d13772625746cdd7fe18c430ac1f37fd4c0089
numlinka <numlinka@163.com>
2025-04-25 12:23:23 +0800

    Fix interface method errors.

--- src/desktop/interface/methods.py
+++ src/desktop/interface/methods.py
@@ -7,16 +7,16 @@ import core
 import interface
 
 
-def save_mainwindow_state(*_):
+def save_mainwindow_state(*_) -> None:
     core.config.window_width = interface.mainwindow.winfo_width()
     core.config.window_height = interface.mainwindow.winfo_height()
     core.config.window_x = interface.mainwindow.winfo_x()
     core.config.window_y = interface.mainwindow.winfo_y()
 
 
-def load_mainwindow_state(*_):
-    width = core.config.window_width if core.config.window_width <= 0 else 650
-    height = core.config.window_height if core.config.window_height <= 0 else 450
+def load_mainwindow_state(*_) -> None:
+    width = core.config.window_width if core.config.window_width > 0 else 650
+    height = core.config.window_height if core.config.window_height > 0 else 450
     interface.mainwindow.geometry(f"{width}x{height}")
 
     if core.config.window_x < 0 or core.config.window_y < 0:
@@ -26,7 +26,7 @@ def load_mainwindow_state(*_):
     interface.mainwindow.geometry(f"+{core.config.window_x}+{core.config.window_y}")
 
 
-def center_window_to_screen(window: tkinter.Misc, width: int = None, height: int = None, set_size: bool = False):
+def center_window_to_screen(window: tkinter.Misc, width: int = None, height: int = None, set_size: bool = False) -> None:
     width = width or window.winfo_width()
     height = height or window.winfo_height()
     screenwidth = window.winfo_screenwidth()
@@ -35,4 +35,8 @@ def center_window_to_screen(window: tkinter.Misc, width: int = None, height: int
     x = int((screenwidth / 2) - (width / 2))
     y = int((screenheight / 2) - (height / 2))
 
-    window.geometry(f"+{x}+{y}")
+    if not set_size:
+        window.geometry(f"+{x}+{y}")
+        return
+
+    window.geometry(f"{width}x{height}+{x}+{y}")
